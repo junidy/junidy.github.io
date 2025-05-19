c0bebe99762f0e8414c8ba32c97734fff6ca29b3
junidy <junidyeom@gmail.com>
2025-05-19 15:49:16 -0400

    Config for github pages

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,6 +3,8 @@ function MainPage() {
     <>
       <h1>Juni Yeom</h1>
       <p>I like building things and making music</p>
+      <p>Exploring isomorphisms between music, narrative, and generative art</p>
+      <p>Understanding the emergent systems that rule us, and how to operate best within them</p>
     </>
   );
 }
